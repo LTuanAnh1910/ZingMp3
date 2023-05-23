bcede69e57c004e2660211633f8f9f326aadc384
Your Name <you@example.com>
2023-05-23 15:46:50 +0700

    PlayerMusic

--- src/store/reducers/musicReducer.js
+++ src/store/reducers/musicReducer.js
@@ -3,6 +3,7 @@ import actionTypes from '../actions/actionTypes';
 
 const initState = {
     curSongId: null,
+    isPlaying: false,
 };
 
 const musicReducer = (state = initState, action) => {
@@ -11,6 +12,12 @@ const musicReducer = (state = initState, action) => {
             return {
                 ...state,
                 curSongId: action.sid || null,
+            };
+
+        case actionTypes.PLAY:
+            return {
+                ...state,
+                isPlaying: action.flag,
             };
 
         default:
